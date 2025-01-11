d0f20223cb87db5c6c9fa27975ed1dcc0cd11d72
kirato01 <you@example.com>
2025-01-11 17:44:47 +0700

    aboutus

--- app/Rooms/page.js
+++ app/Rooms/page.js
@@ -2,25 +2,28 @@ import React from 'react'
 import HotelCard from '@/components/Card/HotelCard'
 import Header from '@components/layout/Header'
 import Footer from '@components/layout/Footer'
+import BannerRoom from '@components/Banners/BannerRoom'
 
 const page = () => {
     return (
         <div className='w-full h-full flex flex-col gap-8'>
-            <div className='w-full flex justify-center items-center shadow-2xl'>
-                <div className='w-10/12'>
-                    {/* Header */}
-                    <Header />
+           <div className='w-full flex flex-col justify-center items-center'>
+                <div className='w-full flex justify-center items-center shadow-2xl'>
+                        <div className='w-10/12'>
+                            {/* Header */}
+                            <Header />
+                        </div>
+                    </div>
 
-                    {/* banner */}
-
-                </div>
-            </div>
-
-            <div className='w-full flex justify-center items-center'>
-                <div className='w-10/12'>
-                    {/* Banner */}
-                </div>
-            </div>
+                    <div className='w-full'>
+                        <div className='w-full flex justify-center items-center'>
+                            <div className='w-full'>
+                                {/* Banner */}
+                                <BannerRoom />
+                            </div>
+                        </div>
+                    </div>
+           </div>
 
             <div className='w-full flex justify-center items-center'>
                 <div className='w-10/12'>
